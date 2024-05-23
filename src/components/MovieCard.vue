c7176fbbffd5edb16bcd363209ca2bf8d9ca88fe
Keren Wigelman <keren.w@gmail.com>
2024-05-23 08:18:52 -0400

    MovieCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,1 +1,44 @@
-<template><div></div></template>
+<template>
+    <div class="movie-card">
+      <h3>{{ movie.title }}</h3>
+      <button @click="toggleWatchlist">
+        {{ isInWatchlist ? 'Remove from Watchlist' : 'Add to Watchlist' }}
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  import { computed } from 'vue';
+  import { useMovieStore } from '../stores/movieStore';
+  
+  export default {
+    props: ['movie'],
+    setup(props) {
+      const movieStore = useMovieStore();
+      const isInWatchlist = computed(() => movieStore.watchlist.includes(props.movie));
+      const toggleWatchlist = () => {
+        if (isInWatchlist.value) {
+          movieStore.removeFromWatchlist(props.movie);
+        } else {
+          movieStore.addToWatchlist(props.movie);
+        }
+      };
+  
+      return {
+        isInWatchlist,
+        toggleWatchlist
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  .movie-card {
+    border: 1px solid #ccc;
+    padding: 16px;
+    margin: 16px;
+    border-radius: 8px;
+    text-align: center;
+  }
+  </style>
+  
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,1 +1,33 @@
-<template><div></div></template>
+<template>
+    <div class="search-bar">
+      <input v-model="query" placeholder="Search for movies..." @input="onSearch" />
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    emits: ['search'],
+    setup(_, { emit }) {
+      const query = ref('');
+  
+      const onSearch = () => {
+        emit('search', query.value);
+      };
+  
+      return {
+        query,
+        onSearch
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  .search-bar {
+    margin: 16px;
+    text-align: center;
+  }
+  </style>
+  
