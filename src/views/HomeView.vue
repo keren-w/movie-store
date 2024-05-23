5a928769ced8c74119c1d89c021b3ba74d750fde
Keren Wigelman <keren.w@gmail.com>
2024-05-23 07:30:56 +0300

    Homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,52 @@
+<template>
+  <div>
+    <SearchBar @search="searchMovies" />
+    <div class="movies">
+      <MovieCard
+        v-for="movie in displayedMovies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+  </div>
+</template>
 
 <script>
-  import { ref, onMounted } from 'vue';
-  import { useMovieStore } from '../stores/movieStore';
-  import MovieCard from '../components/MovieCard.vue';
-  import SearchBar from '../components/SearchBar.vue';
-  
-  export default {
-    components: {
-      MovieCard,
-      SearchBar
-    },
-    setup() {
-      const movieStore = useMovieStore();
-      const displayedMovies = ref(movieStore.popularMovies);
-  
-      const searchMovies = (query) => {
-        displayedMovies.value = query ? movieStore.searchMovies(query) : movieStore.popularMovies;
-      };
-  
-      onMounted(async () => {
-        await movieStore.fetchPopularMovies();
-        displayedMovies.value = movieStore.popularMovies;
-      });
-  
-      return {
-        displayedMovies,
-        searchMovies
-      };
-    }
-  };
-  </script>
+import { ref, onMounted } from 'vue';
+import { useMovieStore } from '../stores/movieStore';
+import MovieCard from '../components/MovieCard.vue';
+import SearchBar from '../components/SearchBar.vue';
+
+export default {
+  components: {
+    MovieCard,
+    SearchBar
+  },
+  setup() {
+    const movieStore = useMovieStore();
+    const displayedMovies = ref(movieStore.popularMovies);
+
+    const searchMovies = (query) => {
+      displayedMovies.value = query ? movieStore.searchMovies(query) : movieStore.popularMovies;
+    };
+
+    onMounted(async () => {
+      await movieStore.fetchPopularMovies();
+      displayedMovies.value = movieStore.popularMovies;
+    });
+
+    return {
+      displayedMovies,
+      searchMovies
+    };
+  }
+};
+</script>
+
+<style>
+.movies {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
