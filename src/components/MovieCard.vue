f246993342c0c6063ae2fec36ce4dc8bbc4ce835
Keren Wigelman <keren.w@gmail.com>
2024-05-26 08:42:58 +0300

    MovieCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,70 +1,118 @@
 <template>
-    <article class="movie-card">
-      <img :src="imgUrl" class="movie-image"/>
-     <section class="movie-info"> 
+  <article class="movie-card" :class="{ 'active': isActive }" @mouseenter="isActive = true"
+    @mouseleave="isActive = false">
+    <img :src="imgUrl" class="movie-image" :class="{ 'active': isActive }" />
+    <div class="watchlist-icon">
+      <Icon name="io-heart" @click="toggleWatchlist" scale="1.5" v-if="isInWatchlist" />
+      <Icon name="io-heart-outline" @click="toggleWatchlist" scale="1.5" v-if="!isInWatchlist" />
+    </div>
+    <section class="movie-info">
       <h3>{{ movie.title }}</h3>
-      <beaker-icon class="icon"  @click="toggleWatchlist"/>
     </section>
-    </article>
-  </template>
-  
-  <script>
-  import { computed } from 'vue';
-  import { useMovieStore } from '../stores/movieStore';
-  import {IMAGE_URL} from '../api/movies-api';
-  import { BeakerIcon } from '@heroicons/vue/24/solid';
-  
-  export default {
-    props: ['movie'],
-    setup(props) {
-      const movieStore = useMovieStore();
-      const isInWatchlist = computed(() => movieStore.watchlist.includes(props.movie));
-      const toggleWatchlist = () => {
-        if (isInWatchlist.value) {
-          movieStore.removeFromWatchlist(props.movie);
-        } else {
-          movieStore.addToWatchlist(props.movie);
-        }
-      };
-  
-      return {
-        isInWatchlist,
-        toggleWatchlist,
-        BeakerIcon,
-        imgUrl: `${IMAGE_URL}/w300${props.movie.poster_path}`
-      };
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .movie-card {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    justify-content: space-between;
-    width: 250px; 
-  }
+  </article>
+</template>
 
-  .movie-image {
-    width: inherit;
-  }
+<script>
+import { computed } from 'vue';
+import { useMovieStore } from '../stores/movieStore';
+import { IMAGE_URL } from '../api/movies-api';
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { IoHeartOutline, IoHeart } from "oh-vue-icons/icons";
+import { ref } from 'vue';
+addIcons(IoHeartOutline, IoHeart);
 
-  .movie-info {
-    padding: 8px 0;
-    width: initial;
-    height: 80px;
+export default {
+  props: ['movie'],
+  setup(props) {
+    const movieStore = useMovieStore();
+    const isInWatchlist = computed(() => movieStore.watchlist.includes(props.movie));
+    const toggleWatchlist = () => {
+      if (isInWatchlist.value) {
+        movieStore.removeFromWatchlist(props.movie);
+      } else {
+        movieStore.addToWatchlist(props.movie);
+      }
+    };
+    const isActive = ref(false);
+    const toggleActive = () => {
+      console.log("toggeled!");
+      isActive.value = !isActive.value;
+    };
+    return {
+      isInWatchlist,
+      toggleWatchlist,
+      isActive,
+      toggleActive,
+      imgUrl: `${IMAGE_URL}/w300${props.movie.poster_path}`
+    };
+  },
+  components: {
+    Icon: OhVueIcon
   }
-
-  h3 {
-    margin: 0;
-  }
-  
-  .icon {
-    width: 36px;
-    height: 36px;
-    color: #222;
-    margin-left: 12px;
 }
-  </style>
-  
+</script>
+
+<style scoped>
+.movie-card {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-between;
+  width: 250px;
+  position: relative;
+
+  &.active {
+    .watchlist-icon {
+      visibility: visible;
+    }
+
+.movie-image {
+  position: relative;
+  &::after {
+    content: "blablabla";
+    visibility: visible;
+    z-index: 10;
+    color: red;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+    h3 {
+      &::before {
+    content: "This content is before.";
+    color: red;
+  }
+  &::after {
+    content: "This content is after.";
+    color: blue;
+  }
+    }
+  }
+}
+
+.movie-image {
+  width: inherit;
+}
+
+.movie-info {
+  padding: 8px 0;
+  width: initial;
+  height: 80px;
+}
+
+h3 {
+  margin: 0;
+
+}
+
+.watchlist-icon {
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  visibility: hidden;
+}
+</style>
